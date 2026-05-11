bd4c23809cb205b770642176cacc9d9d5e1c8075
Kegan McIlwaine <kegan.mcilwaine@gmail.com>
2026-05-10 18:09:32 -0600

    Consolidate some logic

--- Utils/Server.hs
+++ Utils/Server.hs
@@ -4,7 +4,7 @@ import Control.Exception (IOException, catch)
 import Data.Map qualified as Map
 import Data.Set qualified as S
 import ECC.Kernel (emptyContext)
-import Parser.CmdParsers (Command, CommandSpan (CommandSpan, spanRange, spanValue), Range (Range), evalCommand, evalCommandM, parseFile, parseFileSpans)
+import Parser.CmdParsers (Command, CommandSpan (CommandSpan, spanRange, spanValue), Range (Range), evalCommand, evalCommandM, parseFileSpans)
 import Control.Monad (foldM)
 import SessionTypes.Kernel
 import SessionTypes.Tactics (ProofState (..), allSubgoalNames)
@@ -44,19 +44,9 @@ emptyState =
 readFileSafe :: FilePath -> IO String
 readFileSafe path = catch (readFile path) (\e -> let _ = (e :: IOException) in return "")
 
-runProofScript :: FilePath -> String -> IO (Either String ProofState)
-runProofScript fname content = do
-  case parseFileSpans fname content of
-    Left err -> return . Left $ "--------------------------------\nParse Error:\n" ++ show err
-    Right (moduleName, imports, cmdSpans) -> do
-      stateWithImports <- loadImports imports emptyState
-      let s0 = stateWithImports { curModuleName = moduleName }
-      finalState <- foldM (\s sp -> evalCommandM (spanValue sp) s) s0 cmdSpans
-      return . Right $ finalState
-
 loadImports :: [String] -> ProofState -> IO ProofState
-loadImports [] s = return s
-loadImports (m : ms) s = do
+loadImports [] s = pure s
+loadImports (m : ms) s =
   if Map.member m (loadedModules s)
     then loadImports ms s
     else do
@@ -68,16 +58,37 @@ loadImports (m : ms) s = do
           loadImports ms s
         else do
           content <- readFileSafe filename
-          case parseFile filename content of
+          case parseFileSpans filename content of
             Left err -> do
               hPutStrLn stderr $ "[Error] Failed to parse import " ++ m ++ ": " ++ show err
               loadImports ms s
-            Right (subImports, subCmds) -> do
+            Right (_, subImports, subCmds) -> do
               s' <- loadImports subImports s
-              let modState = s' {subgoals = Map.empty, theorems = Map.empty, curModuleName = m, openGoalStack = []}
-              let modResult = foldl (\st f -> f st) modState subCmds
-              let newLoaded = Map.insert m (theorems modResult) (loadedModules s')
+              let modState0 = s' {subgoals = Map.empty, theorems = Map.empty, curModuleName = m, openGoalStack = []}
+                  modResult  = foldl (\st sp -> evalCommand (spanValue sp) st) modState0 subCmds
+                  newLoaded  = Map.insert m (theorems modResult) (loadedModules s')
               loadImports ms (s' {subgoals = Map.empty, theorems = Map.empty, openGoalStack = [], loadedModules = newLoaded})
+
+parseAndLoad
+  :: FilePath -> String
+  -> IO (Either String (String, [String], [CommandSpan Command], ProofState))
+parseAndLoad fname content =
+  case parseFileSpans fname content of
+    Left err ->
+      return . Left $ "--------------------------------\nParse Error:\n" ++ show err
+    Right (moduleName, imports, cmdSpans) -> do
+      stateWithImports <- loadImports imports emptyState
+      let s0 = stateWithImports {curModuleName = moduleName}
+      return $ Right (moduleName, imports, cmdSpans, s0)
+
+runProofScript :: FilePath -> String -> IO (Either String ProofState)
+runProofScript fname content = do
+  res <- parseAndLoad fname content
+  case res of
+    Left err -> return (Left err)
+    Right (_, _, cmdSpans, s0) -> do
+      finalState <- foldM (\s sp -> evalCommandM (spanValue sp) s) s0 cmdSpans
+      return (Right finalState)
 
 -- ==========================================
 -- Detailed Script Execution (with snapshots)
@@ -98,51 +109,21 @@ data ProcessedScript = ProcessedScript
 
 runProofScriptDetailed :: FilePath -> String -> IO (Either String ProcessedScript)
 runProofScriptDetailed fname content = do
-  case parseFileSpans fname content of
-    Left err ->
-      pure . Left $ "--------------------------------\nParse Error:\n" ++ show err
-    Right (moduleName, imports, cmds) -> do
-      stateWithImports <- loadImportsDetailed imports emptyState
-      let initialState = stateWithImports {curModuleName = moduleName}
-          step (st, acc) sp = do
+  res <- parseAndLoad fname content
+  case res of
+    Left err -> return (Left err)
+    Right (moduleName, imports, cmds, initialState) -> do
+      let step (st, acc) sp = do
             st' <- evalCommandM (spanValue sp) st
-            let snap = Snapshot {beforeState = st, afterState = st'}
-            return (st', acc ++ [snap])
+            return (st', acc ++ [Snapshot {beforeState = st, afterState = st'}])
       (_, snaps) <- foldM step (initialState, []) cmds
-      pure $
-        Right
-          ProcessedScript
-            { psModuleName = moduleName,
-              psImports = imports,
-              psCommands = cmds,
-              psSnaps = snaps
-            }
-
-loadImportsDetailed :: [String] -> ProofState -> IO ProofState
-loadImportsDetailed [] s = pure s
-loadImportsDetailed (m : ms) s =
-  if Map.member m (loadedModules s)
-    then
-      loadImportsDetailed ms s
-    else do
-      let filename = m ++ ".still"
-      exists <- doesFileExist filename
-      if not exists
-        then do
-          hPutStrLn stderr $ "[Warning] Import not found: " ++ filename
-          loadImportsDetailed ms s
-        else do
-          content <- readFileSafe filename
-          case parseFileSpans filename content of
-            Left err -> do
-              hPutStrLn stderr $ "[Error] Failed to parse import " ++ m ++ ": " ++ show err
-              loadImportsDetailed ms s
-            Right (_, subImports, subCmds) -> do
-              s' <- loadImportsDetailed subImports s
-              let modState0 = s' {subgoals = Map.empty, theorems = Map.empty, curModuleName = m, openGoalStack = []}
-                  modResult = foldl (\st sp -> evalCommand (spanValue sp) st) modState0 subCmds
-                  newLoaded = Map.insert m (theorems modResult) (loadedModules s')
-              loadImportsDetailed ms (s' {subgoals = Map.empty, theorems = Map.empty, openGoalStack = [], loadedModules = newLoaded})
+      return . Right $
+        ProcessedScript
+          { psModuleName = moduleName
+          , psImports    = imports
+          , psCommands   = cmds
+          , psSnaps      = snaps
+          }
 
 -- ==========================================
 -- Cursor Position Lookup
